36d515271026ae3e646a3494ca0cc18da1fb2e30
UVM-PUEBLA <UVM-PUEBLA@DESKTOP-5VJHBI8>
2019-10-28 17:35:22 -0600

    Ana y Ani hicieron la clase Product

---
+++ Ahome.Web/Data/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ahome.Web.Data.Entities
+{
+    public class Product
+    {
+       [Key]
+       public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [MaxLength(50,ErrorMessage = "El campo {0} debe máximo {1} caracteres")]
+        [Display(Name = "Producto")]
+        public string Name { get; set; }
+    }
+}
