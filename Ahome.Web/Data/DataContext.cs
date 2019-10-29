cbc47bb5a40ac84ea8d7b0ddfd9e99d7d99ee8cb
contreras.florirais@hotmail.com <contreras.florirais@hotmail.com>
2019-10-28 22:48:17 -0600

    Areli y Flor hicieron la clase datacontext.cs

---
+++ Ahome.Web/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Ahome.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ahome.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
